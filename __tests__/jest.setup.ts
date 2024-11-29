8884cff9ae286f7c5a9765112361f93d09f996db
Maberick2 <rramirezcarmona83@gmail.com>
2024-11-29 06:07:20 -0700

    gnr

--- __tests__/jest.setup.ts
+++ __tests__/jest.setup.ts
@@ -2,8 +2,8 @@ import { jest } from '@jest/globals';
 
 jest.setTimeout(30000);
 
-// Asegura que las variables de entorno necesarias estén definidas
+process.env.GITHUB_REPOSITORY =
+  process.env.GITHUB_REPOSITORY || 'docker/login-action';
 process.env.RUNNER_TEMP = process.env.RUNNER_TEMP || '/tmp/github_runner';
 process.env.RUNNER_TOOL_CACHE =
   process.env.RUNNER_TOOL_CACHE || '/tmp/github_tool_cache';
-process.env.NODE_ENV = 'test';
